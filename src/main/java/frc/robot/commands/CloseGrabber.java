91a8c1595ab2c60b03e3d5371076261f3ed7608f
CID <mschoifet@friendscentral.org>
2023-02-23 17:33:43 -0500

    basic grabber code

---
+++ src/main/java/frc/robot/commands/CloseGrabber.java
@@ -0,0 +1,38 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.GrabberSubsystem;
+
+/**
+ * An example command that uses an example subsystem.
+ */
+public class CloseGrabber extends CommandBase
+{
+
+  private final GrabberSubsystem grabberSubsystem;
+
+  /**
+   * Extend arm to given distance in meters
+   * might not matter if it is a cone or a cube but... it's there if it is needed
+   */
+  public CloseGrabber(GrabberSubsystem g)
+  {
+    grabberSubsystem = g;
+  }
+
+  @Override
+  public void initialize() {
+    grabberSubsystem.clamp();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished()
+  {
+    return true;
+  }
+}
