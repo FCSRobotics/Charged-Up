4b2ee8b09037e91f99b6e7067b69052839b8fe5f
CID <mschoifet@friendscentral.org>
2023-02-23 18:18:10 -0500

    just saving changes... need to finish the rotating of the arm and change the pid stuff to use the pid controller on the spark maxes

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -5,6 +5,7 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.CAN;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.Arm;
 
@@ -16,32 +17,50 @@ public class ArmSubsystem extends SubsystemBase
 {
 
   public final CANSparkMax extendSparkMax;
+  public final CANSparkMax rotateSparkMax;
   public final RelativeEncoder extendEncoder;
+  public final RelativeEncoder rotatingEncoder;
   public double desiredDistance;
   public double currentDistance;
-  public final PIDController pid;
+  public double desiredHeight;
+  public double currentHeight;
+  public final PIDController extensionPID;
+  public final PIDController rotatingPID;
 
-  public ArmSubsystem(int extendSparkMaxId, float revToMetersConversionFactor) {
+  public ArmSubsystem(int extendSparkMaxId, int rotateSparkMaxId, float revToMetersConversionFactor) {
     extendSparkMax = new CANSparkMax(extendSparkMaxId, MotorType.kBrushless);
+    rotateSparkMax = new CANSparkMax(rotateSparkMaxId, MotorType.kBrushless);
+
     extendEncoder = extendSparkMax.getEncoder();
     extendEncoder.setPositionConversionFactor(revToMetersConversionFactor);
     extendEncoder.setVelocityConversionFactor(revToMetersConversionFactor);
+    rotatingEncode = rotateSparkMax.getEncoder();
+    rotatingEncoder.setPositionConversionFactor(revToMetersConversionFactor);
+
     desiredDistance = 0;
     currentDistance = extendEncoder.getPosition();
-    pid = new PIDController(Arm.p, Arm.i, Arm.d);
+    extensionPID = new PIDController(Arm.pExtension, Arm.iExtension, Arm.dExtension);
+    rotatingPID = new PIDController(Arm.pRotating, Arm.iRotating, Arm.dRotating);
   }
 
   @Override
   public void periodic() {
     if (Math.abs(desiredDistance - currentDistance) > 0.02) {
       currentDistance = extendEncoder.getPosition();
-      extendSparkMax.set(pid.calculate(currentDistance, desiredDistance));
+      extendSparkMax.set(extensionPID.calculate(currentDistance, desiredDistance)); // might want to change this to the built in one
+    }
+    if (Math.abs(desiredHeight - currentHeight) > 1) {
+      currentHeight = rotatingEncoder.getPosition();
     }
   }
 
   public void setDesiredDistance(double distance) {
     desiredDistance = distance;
   } 
+
+  public void setDesiredHeight(double height) {
+    desiredHeight = height;
+  }
 
   // public void extendToDistanceMeters(float distance) {
       
